39543a52951e59f593583751f88fd0be8d3aa64e
hi-crystal <75575215+hi-crystal@users.noreply.github.com>
2021-01-31 22:06:29 -0500

    Implementing 5-Day Forecast

--- Final Project/src/index.js
+++ Final Project/src/index.js
@@ -21,23 +21,51 @@ function formatDate(today) {
   return `${day} ${currentHour}:${currentMinutes}`;
 }
 
+//5-day forecast display
+function displayForecast(response) {
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = null;
+  let forecast = null;
+
+  for (let index = 0; index < 5; index++) {
+    forecast = response.data.daily[index];
+    console.log(forecast);
+    forecastElement.innerHTML += `
+    <div class="col">
+    <div class="card text-center h-100">
+    <img src="images/sun.svg" class="card-img-top" alt="">
+      <div class="card-title" id="day">${forecast.dt}
+        </div>
+        <div class="card-body" id="range">
+        <div id="high">${Math.round(forecast.temp.max)}</div>
+        <div id="low">${Math.round(forecast.temp.min)}</div>
+        </div>
+    </div>
+  </div>
+      `
+    }
+  }
+
 //Temp display in default units
 function displayTemp(response) {
   let currentTemp = Math.round(response.data.main.temp);
-  let defaultUnit = "°C";
+  let unit = "metric";
+  let unitDisplay = "°C";
   let city = response.data.name;
-  document.querySelector("p").innerHTML = `It's currently ${currentTemp}${defaultUnit} in ${city}`;
+  let latitude = response.data.coord.lat;
+  let longitude = response.data.coord.lon;
+  apiUrl = `${apiEndpoint}onecall?lat=${latitude}&lon=${longitude}&exclude=minutely,hourly&appid=${apiKey}&units=${unit}`;
+
+  document.querySelector("p").innerHTML = `It's currently ${currentTemp}${unitDisplay} in ${city}`;
+  axios.get(apiUrl).then(displayForecast);
+  console.log(apiUrl);
 }
 
 // Location entry form
 function searchByCity(event) {
   event.preventDefault();
   let city = document.querySelector("#location-input").value.trim();
-  let units = "metric";
-  let apiKey = "ca919d3d566d6ae96426df805fb208b2";
-  let apiEndpoint = "https://api.openweathermap.org/data/2.5/weather?";
-  let apiUrl = `${apiEndpoint}q=${city}&appid=${apiKey}&units=${units}`;
-
+  let apiUrl = `${apiEndpoint}weather?q=${city}&appid=${apiKey}&units=${units}`;
   if (city !== " ") {
     axios.get(apiUrl).then(displayTemp);
   } else {
@@ -54,10 +82,7 @@ function searchByCoords(event) {
   function showPosition(position) {
     let latitude = position.coords.latitude;
     let longitude = position.coords.longitude;
-    let units = "metric";
-    let apiKey = "ca919d3d566d6ae96426df805fb208b2";
-    let apiEndpoint = "https://api.openweathermap.org/data/2.5/weather?";
-    let apiUrl = `${apiEndpoint}lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=${units}`;
+    let apiUrl = `${apiEndpoint}weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=${units}`;
 
     axios.get(apiUrl).then(displayTemp);
   }
@@ -76,6 +101,10 @@ function serveFarenheit(event) {
 let currentTime = new Date();
 let todaysDate = document.querySelector("#local-time");
 todaysDate.innerHTML = formatDate(currentTime);
+
+let apiKey = "ca919d3d566d6ae96426df805fb208b2";
+let apiEndpoint = "https://api.openweathermap.org/data/2.5/";
+let units = "metric";
 
 let locationForm = document.querySelector("#location-form");
 locationForm.addEventListener("submit", searchByCity);
